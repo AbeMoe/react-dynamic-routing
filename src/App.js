c8a77115aeb80142232e4b77813eb84793f8a1eb
AbeMoe <40703430+AbeMoe@users.noreply.github.com>
2020-04-11 13:53:40 -0700

    Implemented dynamic routing+reactive changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
+
+
+import axios from 'axios';
 function App() {
+  const [data, setData] = useState({ hits: [] });
+  useEffect(() => {//Here we're grabbing data from our API
+    const fetchData = async () => {
+      const result = await axios(
+        'https://hn.algolia.com/api/v1/search?query=redux', //This could be your firebase api route
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/*This creates our links to our routes,notice how all of our links are created from our fetchData function from earlier*/}
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <Link to={{pathname:'/'+item.objectID,state:{item:item}}}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+      {/* This routes says "When the user requests a given path, display it with these props*/}
+      <Switch>
+        <Route path="/:id" render={props=>
+          (<Childs {...props}/>)}/>       
+      </Switch>
+    </Router>
   );
 }
+
+//This class is what creates and updates our routes orios
+class Childs extends React.Component {
+  state = {
+    item: 'asdasdsd'
+  }
+  componentDidMount () {
+    const { item } = this.props.location.state
+    this.setState(() => ({ item }))
+  }
+  render() {
+
+
+    return(<h1>{this.props.location.state.item.title}</h1>)
+  }
+}
+
 
 export default App;
